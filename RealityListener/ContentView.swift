7341572f99c4c179ef39d78452c6b5258fbd6949
Morten Just <morten.just@genstart.dk>
2021-05-21 11:56:13 +0200

    add both speakers

--- RealityListener/ContentView.swift
+++ RealityListener/ContentView.swift
@@ -18,17 +18,43 @@ struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
         
+        
         let arView = ARView(frame: .zero)
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let speaker01 = try! Experience.loadSpeaker01()
+        let speaker02 = try! speaker01.findEntity(named: "Speaker02")!
+        
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(speaker01)
+        
+        addAudio(withName: "steve01.wav", to: speaker01)
+        addAudio(withName: "woz01.wav", to: speaker02)
+  
         
         return arView
         
     }
+    
+    func addAudio(withName name: String, to entity : Entity) {
+        
+        do {
+            let resource = try AudioFileResource.load(named: name)
+            resource.loadingStrategy = .preload
+            resource.shouldLoop = true
+            resource.inputMode = .spatial
+            
+            let audioController = entity.prepareAudio(resource)
+            
+            print("playing")
+            audioController.play()
+            
+        } catch {
+            print("can't load: ", error.localizedDescription)
+        }
+    }
+    
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
